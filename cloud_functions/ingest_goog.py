035fd9e0bf8c031283537f70e846536d9ce8e4e4
matthh9797 <matthewh@netextremity.com>
2022-04-17 13:59:39 +0100

    Finished ingest method

--- cloud_functions/ingest_goog.py
+++ cloud_functions/ingest_goog.py
@@ -1,13 +1,15 @@
 import os
 import logging
 import yfinance as yf
+import tempfile  # Used to create temporary directories/files
+import shutil
 
 from google.cloud import storage
 from google.cloud.storage import Blob
 from google.cloud import bigquery
 
 
-def download(date: str, destdir: str):
+def download(date: str, destdir: str, full_refresh: bool):
     """
     Downloads Google ticker market close data and returns local file name
     date e.g. '2019-04-05'
@@ -16,9 +18,12 @@ def download(date: str, destdir: str):
     try:
         goog = yf.Ticker("GOOG")
         history = goog.history(period="max")
-        snapshot = history.loc[[date]].reset_index()
+        if full_refresh is True:
+            history = history.loc[:date].reset_index()
+        else:
+            history = history.loc[[date]].reset_index()
         filename = os.path.join(destdir, "{}".format(date))
-        snapshot.to_csv(filename)
+        history.to_csv(filename, index=False)
         logging.debug("{} saved".format(filename))
         return filename
     except KeyError:
@@ -43,17 +48,61 @@ def upload(csvfile, bucketname, blobname):
     return gcslocation
 
 
-def bqload(gcsfile, date):
+def bqload(gcsfile: str, full_refresh: bool):
     """
     Loads the csv file in GCS to BigQuery, replacing data in the
     current partition
     """
     client = bigquery.Client()
-    table_ref = client.dataset("gcp_stock_analysis")
+    table_ref = client.dataset("gcp_stock_analysis").table("stock_price")
+    job_config = bigquery.LoadJobConfig()
+    job_config.source_format = "CSV"
+    if full_refresh is True:
+        job_config.write_disposition = "WRITE_TRUNCATE"
+    else:
+        job_config.write_disposition = "WRITE_APPEND"
+    job_config.ignore_unknown_values = True
+    job_config.skip_leading_rows = 1
+    # job_config.autodetect = True
+    job_config.schema = [
+        bigquery.SchemaField(
+            col_and_type.split(":")[0], col_and_type.split(":")[1]
+        )  # , mode='required')
+        for col_and_type in [
+            "Date:DATE",
+            "Open:FLOAT64",
+            "High:FLOAT64",
+            "Low:FLOAT64",
+            "Close:FLOAT64",
+            "Volume:NUMERIC",
+            "Dividend:NUMERIC",
+            "Stock_Splits:NUMERIC",
+        ]
+    ]
+    load_job = client.load_table_from_uri(gcsfile, table_ref, job_config=job_config)
+    load_job.result()
+
+    if load_job.state != "DONE":
+        raise load_job.exception()
+
+    return table_ref, load_job.output_rows
 
 
-def ingest(date, bucket):
-    """Ingest data into bucket"""
+def ingest(date: str, bucket: str, full_refresh=False):
+    """Ingest data from yahoo finance api into BigQuery
+    return table, numrows on success
+    """
+    tempdir = tempfile.mkdtemp(prefix="ingest_stocks")
+    try:
+        file = download(date, "repository", full_refresh=full_refresh)
+        if file is not None:
+            gcsloc = "stocks/raw/goog/{}.csv".format(date)
+            gcsloc = upload(file, bucket, gcsloc)
+            return bqload(gcsloc, full_refresh=full_refresh)
+
+    finally:
+        logging.debug("Cleaning up by removing {}".format(tempdir))
+        shutil.rmtree(tempdir)
 
 
 def next_date():
