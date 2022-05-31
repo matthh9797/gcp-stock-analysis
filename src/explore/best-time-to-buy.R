7971fae99c393ab84404e5bb4d4a339d052d5a2f
matthh9797 <matthewh@netextremity.com>
2022-05-31 10:36:59 +0100

    Added day of month analysis

--- src/explore/best-time-to-buy.R
+++ src/explore/best-time-to-buy.R
@@ -42,6 +42,12 @@ lw1 <- loess(close ~ as.numeric(date), data = kr, span = 0.1)
 lw2 <- loess(close ~ as.numeric(date), data = kr, span = 0.15)
 lw3 <- loess(close ~ as.numeric(date), data = kr, span = 0.05)
 
+# lw1
+lw1_open <- loess(open ~ as.numeric(date), data = kr, span = 0.1)
+lw1_high <- loess(high ~ as.numeric(date), data = kr, span = 0.1)
+lw1_low <- loess(low ~ as.numeric(date), data = kr, span = 0.1)
+lw1_close <- loess(close ~ as.numeric(date), data = kr, span = 0.1)
+
 kr %>%
   gather_predictions(lw1, lw2, lw3) %>%
   ggplot(aes(date, close, colour=model)) +
@@ -53,28 +59,54 @@ kr %>%
   ggplot(aes(date, resid)) +
   geom_line()
 
-kr <- kr %>%
-  kr %>%
-ggplot(aes(month, resid, group=month)) +
-geom_boxplot()
-add_residuals(lw1) %>%
-  mutate(dayofweek = wday(date, label = TRUE),
-
+(kr_resid <- kr %>%
+    gather_residuals(lw1_open, lw1_high, lw1_low, lw1_close) %>% 
+    select(-open, -high, -low, -close, -adjusted, -volume))
 
 kr %>%
   ggplot(aes(month, resid, group=month)) +
   geom_boxplot()
 
+# The data suggest that the lowest price on an arbitary day
+# is on a Thursday. But what time on a Thursday?
 kr %>%
   ggplot(aes(dayofweek, resid)) +
   geom_boxplot()
 
-# The data suggest that the lowest price on an arbitary day
-# is on a Thursday. But what time on a Thursday?
 kr %>%
   group_by(dayofweek) %>%
   summarise(across(
     .cols = c(open, high, low, close),
+    .fns = list(mean = mean, md = median, sd = sd)
+  )) 
+
+kr_resid %>%
+  group_by(model, dayofweek) %>%
+  summarise(across(
+    .cols = c(resid),
+    .fns = list(mean = mean, md = median, sd = sd)
+  )) 
+
+# The data suggest that the lowest price on an arbitary day
+# is on a Thursday. But what time on a Thursday?
+# This analysis indicates you should by stock on the first Thursday of teh week
+kr_resid %>%
+  filter(model == "lw1_close") %>% 
+  ggplot(aes(dayofmonth, resid, group = dayofmonth)) +
+  geom_boxplot()
+
+kr %>%
+  group_by(dayofmonth) %>%
+  summarise(across(
+    .cols = c(open, high, low, close),
+    .fns = list(mean = mean, md = median, sd = sd)
+  )) %>% 
+  view()
+
+kr_resid %>%
+  group_by(model, dayofmonth) %>%
+  summarise(across(
+    .cols = c(resid),
     .fns = list(mean = mean, md = median, sd = sd)
   ))
 
